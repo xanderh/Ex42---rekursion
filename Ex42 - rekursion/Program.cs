6c38ae44c4fc78c55452660d944891212204a017
Alexander Hvilshøj <alexander@hvilshoej.dk>
2018-03-16 09:32:02 +0100

    Full solution, including tests and documentation

--- Ex42 - rekursion/Program.cs
+++ Ex42 - rekursion/Program.cs
@@ -6,10 +6,65 @@ using System.Threading.Tasks;
 
 namespace Ex42___rekursion
 {
-    class Program
+    /// <summary>
+    /// Main program class
+    /// </summary>
+    public class Program
     {
         static void Main(string[] args)
         {
+            int factorial = 3;
+            Console.WriteLine("Factorial of " + factorial + " is " + Factorial(factorial));
+            int fibonacci = 5;
+            Console.WriteLine("Fibonacci number of " + fibonacci + " is " + Fibonacci(fibonacci));
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Calculates the factorial value of any positive integer. 
+        /// </summary>
+        /// <param name="value">Throws an ArgumentOutOfRangeException if value is less than 1</param>
+        /// <returns>The integer corresponding to the factorial of value</returns>
+        public static int Factorial(int value)
+        {
+            if (value < 1)
+            {
+                throw new ArgumentOutOfRangeException("Value must be greater than zero");
+            }
+            if (value == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return value * Factorial(value - 1);
+            }
+        }
+
+        /// <summary>
+        /// Calculates the Fibonacci number, assuming f(0) is 0, and f(1) is 1
+        /// </summary>
+        /// <param name="value">Throws an ArgumentOutOfRangeException if value is negative</param>
+        /// <returns>Integer corresponding to f(value)</returns>
+        public static int Fibonacci(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentOutOfRangeException("Value must be non-negative");
+            }
+            else if (value == 0)
+            {
+                return 0;
+            }
+            else if (value == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fibonacci(value - 1) + Fibonacci(value - 2);
+            }
         }
     }
 }
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Ex42___rekursion;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestFactorial()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => Program.Factorial(0));
+            Assert.AreEqual(1, Program.Factorial(1));
+            Assert.AreEqual(2, Program.Factorial(2));
+            Assert.AreEqual(6, Program.Factorial(3));
+        }
+
+        [TestMethod]
+        public void TestFibonacci()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => Program.Fibonacci(-1));
+            Assert.AreEqual(0, Program.Fibonacci(0));
+            Assert.AreEqual(1, Program.Fibonacci(1));
+            Assert.AreEqual(2, Program.Fibonacci(3));
+            Assert.AreEqual(5, Program.Fibonacci(5));
+            Assert.AreEqual(8, Program.Fibonacci(6));
+        }
+    }
+}
